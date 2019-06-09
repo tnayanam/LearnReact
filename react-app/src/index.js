c4eeb69db30d00907cebcf912af0d1dc63681b9d
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-09 17:56:42 -0400

    Adding Modules

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,4 @@
-class Person{
-constructor(name){
-    this.name = name;
-}
-    Walk(){
-        console.log("walk");
-    }
-}
-
-class Teacher extends Person{
-    constructor(name, degree){
-        super(name);
-        this.degree = degree;
-    };
-    teach(){
-        console.log("teach");
-    }
-}
+import {Teacher} from './Teacher';
 
 const teacher = new Teacher('Tanuj', 'Csc');
 console.log(teacher);
