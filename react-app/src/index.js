63873a1029212eb09fd171689e3d87d4ab3b40a7
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-09 17:26:43 -0400

    Added class

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,20 +1,11 @@
-// Spread Operator
-const first = [1,2,3];
-const second = [4,5,6];
+class Person{
+constructor(name){
+    this.name = name;
+}
+    Walk(){
+        console.log(this);
+    }
+}
 
-// const combined =  first.concat(second); bad way
-const combined = [...first ,'a',...second]; // output array like 1 ,2 ,3 a, 4 ,5 ,6
+const person = new Person("tanuj");
 
-
-// lets combine two objects
-
-const test2 = {
-    job: 'instrucotr'
-};
-const test1 = {
-    name: 'Mosh'
-};
-
-const comb = {...test1 , ...test2};
-
-console.log(comb);  // Output - {name: "Mosh", job: "instrucotr"}
