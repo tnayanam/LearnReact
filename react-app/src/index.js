75de4eb37453ea00e2d48220f5a0154a542c238d
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-09 17:14:06 -0400

    Added spread operator usage

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,20 @@
-// Object Destructuring
+// Spread Operator
+const first = [1,2,3];
+const second = [4,5,6];
 
-const address =  {
-street: '',
-city:'',
-country:''
+// const combined =  first.concat(second); bad way
+const combined = [...first ,'a',...second]; // output array like 1 ,2 ,3 a, 4 ,5 ,6
+
+
+// lets combine two objects
+
+const test2 = {
+    job: 'instrucotr'
+};
+const test1 = {
+    name: 'Mosh'
 };
 
-// bad way
-// const street = address.street;
-// const city = address.city;
-// const country = address.country;
-//---------------------------
-// const  {street, city, country} = address; 
-// ------------------------------
-// const  {street} = address;
+const comb = {...test1 , ...test2};
 
-// ----------------------------
-const{street: st} = address;
-
-
+console.log(comb);  // Output - {name: "Mosh", job: "instrucotr"}
