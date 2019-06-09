52cbc48fa5f94996b2232244d4d633b30def4265
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-09 15:32:08 -0400

    this keyword and its problem in javascript

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,23 +1,14 @@
-// // import React from 'react';
-// // import ReactDOM from 'react-dom';
-
-// // const element = <h1>Hello World</h1>
-// // ReactDOM.render(element, document.getElementById('root')); // here we are adding this element to that root element in main DOM.
-
-
-// function sayHello(){
-//     for(let i = 0 ; i < 5; i++)
-//     console.log(i);
-//     console.log(i); // now here we are getting error as i is not defined. so scope of i is limited the loop 
-// }
-
-// sayHello();
-// Objects in javascript are collection of key value pairs.
 const person = {
-name: 'Mosh', // one property
-walk: function(){}, // one function
-talk(){}            // another way to add a function
+    name: "Mosh",
+    walk(){
+            console.log(this);
+    }
 };
 
-person.talk();
-person.name = 'John';
+person.walk(); // here this returns the actual object
+
+var test = person.walk;
+
+test(); //here this returns the windows object
+
+
