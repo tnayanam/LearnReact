8397499c41aa4d3ee6b888f63a127e6bfc5fe572
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-10 13:13:00 -0400

    Adding Styles

--- react-app/React/counter-app/src/components/counter.jsx
+++ react-app/React/counter-app/src/components/counter.jsx
@@ -4,10 +4,14 @@ class Counter extends Component {
   state = {
     count: 0
   };
+  styles ={
+      fontSize:10,
+      fontWeight: 'bold'
+  };
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style ={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
