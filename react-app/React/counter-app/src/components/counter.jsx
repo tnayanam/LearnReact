fec02d32d75e91cc7fd133ec236c5ff57b11f1d8
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-10 13:55:08 -0400

    Event Binding

--- react-app/React/counter-app/src/components/counter.jsx
+++ react-app/React/counter-app/src/components/counter.jsx
@@ -2,19 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags:["tag1", "tag2", "tag3"]
+    count: 1
   };
   render() {
     return (
       <React.Fragment>
-       {this.renderTags()}
+        <span style={{ fontSize: 10, fontWeight: "bold" }}className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick = {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
-  renderTags(){
-      if(this.state.tags.length === 0) return <p>There are no tags!</p>;
-      return  <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>;
+
+  handleIncrement = () => {
+      console.log("Increment button clicked", this);
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
