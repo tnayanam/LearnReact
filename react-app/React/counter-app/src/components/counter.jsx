e5f456ed487cb75b14a9568505c850392b59c25d
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-10 13:10:07 -0400

    Add class to the element

--- react-app/React/counter-app/src/components/counter.jsx
+++ react-app/React/counter-app/src/components/counter.jsx
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imgUrl: "https://picsum.photos/200"
+    count: 0
   };
   render() {
     return (
       <React.Fragment>
-          <img src = {this.state.imgUrl}/>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
