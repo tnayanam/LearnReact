30990e5f1238831d9d13db79177de90171bafccf
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-09 22:44:48 -0400

    Render dynamic content

--- react-app/React/counter-app/src/components/counter.jsx
+++ react-app/React/counter-app/src/components/counter.jsx
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return  (<React.Fragment> <h1>Hello World</h1><button>Increment</button></React.Fragment>);
-    }
+  state = {
+    count: 1
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <h1>{this.formatCount()}</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
 }
- 
+
 export default Counter;
