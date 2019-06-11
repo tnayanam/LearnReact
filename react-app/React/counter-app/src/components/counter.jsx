3c3c10c949d26e5470c9f9b4ea7c1b6ec14f29a0
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-10 23:31:42 -0400

    Interaction between components

--- react-app/React/counter-app/src/components/counter.jsx
+++ react-app/React/counter-app/src/components/counter.jsx
@@ -2,21 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 0
   };
   render() {
     return (
       <React.Fragment>
-        <span style={{ fontSize: 10, fontWeight: "bold" }}className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick = {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button> 
+        <span
+          style={{ fontSize: 10, fontWeight: "bold" }}
+          className={this.getBadgeClasses()}
+        >
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {/* since we are using function reference this.handleIncrement so we cannot pass argument, in order to pass argument we would need to do things differetly coz we cannot use this.handleIncrement (some value) */}
       </React.Fragment>
     );
   }
 
   handleIncrement = () => {
-      this.setState({count: this.state.count + 1})
-  }
+    this.setState({ count: this.state.count + 1 });
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
