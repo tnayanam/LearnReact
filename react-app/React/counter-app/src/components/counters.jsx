6188fcf53791b44b8c525edb40587c98ff53f7ae
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-14 11:26:22 -0400

    State vs Prop - just read the comments in this commit no code

--- react-app/React/counter-app/src/components/counters.jsx
+++ react-app/React/counter-app/src/components/counters.jsx
@@ -25,3 +25,10 @@ class Counters extends Component {
 }
 
 export default Counters;
+
+/* Differece between props and state 
+prop is properties that is passed to a component and it cannot be altered.
+in this example counters.jsx passed property to counter component, where as the state is local to a counter. so if you want to alter a prop in a counter you need to set it to a state and then change it.
+ 
+*/
+
