92aa8a5b1caa9f5dc65965be629609ba8350ba03
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-14 14:02:51 -0400

    Delete functionality working

--- react-app/React/counter-app/src/components/counters.jsx
+++ react-app/React/counter-app/src/components/counters.jsx
@@ -4,21 +4,27 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 }, 
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
   };
 
-  handleDelete = (counterId) =>
-  {
-      console.log('Event Handler Called', counterId);
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id != counterId);
+    this.setState({ counters: counters });
   };
   render() {
     return (
       <div>
-        {this.state.counters.map(counter => (<Counter key={counter.id} value={counter.value} onDelete = {this.handleDelete} id = {counter.id}/>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            onDelete={this.handleDelete}
+            id={counter.id}
+          />
         ))}
       </div>
     );
@@ -32,4 +38,3 @@ prop is properties that is passed to a component and it cannot be altered.
 in this example counters.jsx passed property to counter component, where as the state is local to a counter. so if you want to alter a prop in a counter you need to set it to a state and then change it.
 
 */
-
