516fd91977feb1c04f178707b1cdfb722b4ce990
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-14 14:09:03 -0400

    added some optimizations for delete

--- react-app/React/counter-app/src/components/counters.jsx
+++ react-app/React/counter-app/src/components/counters.jsx
@@ -21,9 +21,8 @@ class Counters extends Component {
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            value={counter.value}
             onDelete={this.handleDelete}
-            id={counter.id}
+            counter={counter}
           />
         ))}
       </div>
