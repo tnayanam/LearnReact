1d66e957a02b89ac1e30d71a62d2bd87babfbcdd
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-24 20:11:32 -0400

    the main component is shifted to App parent

--- react-app/React/counter-app/src/App.js
+++ react-app/React/counter-app/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import './App.css';
-import Counters from './components/counters';
-import NavBar from './components/navbar';
+import React, { Component } from "react";
+import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
-function App() {
-  return (
-    <React.Fragment>
-<NavBar/>
-<Counters/>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Counters counters = {this.state.counters} onReset = {this.handleReset} onIncrement = {this.handleIncrement} onDelete = {this.handleDelete}/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
