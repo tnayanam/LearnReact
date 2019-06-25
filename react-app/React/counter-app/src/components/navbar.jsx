e04ace755261fef46023c820f997704a6822e5a5
Tanuj Nayanam <tnayanam@sessionm.com>
2019-06-24 20:24:48 -0400

    Removed class and used stateless functional component

--- react-app/React/counter-app/src/components/navbar.jsx
+++ react-app/React/counter-app/src/components/navbar.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill bandge-secondary">{this.props.totalCounters}</span>
-        </a>
-      </nav>
-    );
-  }
-}
+// converting to stateless fucntional component
 
+const NavBar = (props) => {
+    return (   <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      Navbar
+      <span className="badge badge-pill bandge-secondary">{props.totalCounters}</span>
+    </a>
+  </nav> );
+}
+ 
 export default NavBar;
