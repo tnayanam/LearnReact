22af9003abb77024e50184902307ab81888164ce
Tanuj Nayanam <tnayanam@sessionm.com>
2019-07-01 12:11:58 -0400

    Add a like component and then import it

---
+++ vidly/src/components/common/like.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// 
+class Like extends Component {
+    render() {   return ( <i class="fa fa-heart-o" aria-hiddem ="true"></i> );
+    }
+}
+ 
+export default Like;
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -23,7 +24,7 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
-              <th />
+              <th /> <th />
             </tr>
           </thead>
           <tbody>
@@ -32,7 +33,10 @@ class Movies extends Component {
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
+                <td>{movie.dailyRentalRate}</td> 
+                 <td>
+                   <Like/>
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
@@ -41,6 +45,7 @@ class Movies extends Component {
                     Delete
                   </button>
                 </td>
+              
               </tr>
             ))}
           </tbody>
